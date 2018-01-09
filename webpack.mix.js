69725c86f8621d6a1f449cb1ae5cc4d1b24099e2
Segundo Fdez <segun.2@gmail.com>
2018-01-09 23:52:39 +0100

    delete xtra files and change structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,5 @@
 let mix = require('laravel-mix');
-let ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,8 +8,10 @@ const imageminMozjpeg = require('imagemin-mozjpeg');
  |
  */
 
-mix.js('src/js/main.js', 'public/js')
-    .less('src/styles/main.less', 'public/css')
+mix.less('less/sia.less', 'css', {
+    outputStyle: mix.inProduction ? 'compressed' : 'expanded'
+})
+/*
     .options({
         postCss: [
             require('autoprefixer')({
@@ -20,35 +20,7 @@ mix.js('src/js/main.js', 'public/js')
         ]
     })
     .combine([
-        'node_modules/normalize.css/normalize.css',
-        'public/css/main.css'],
-        'public/css/main.min.css')
-    .browserSync({
-        proxy: '0.0.0.0:8181',
-        files: ['public/*.html', 'public/css/*.css', 'public/js/*.js']
-    })
+        'public/css/sia.css'],
+        'public/css/sia.min.css')
 ;
-
-//Only in production, you can remove if you want to use "npm run watch"
-if(mix.inProduction()) {
-    mix.webpackConfig({
-        plugins: [
-            new CopyWebpackPlugin([{
-                from: 'src/img',
-                to: 'public/img/',
-            }]),
-            new ImageminPlugin({
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                pngquant: {
-                    quality: '65-80'
-                },
-                plugins: [
-                    imageminMozjpeg({
-                        quality: 65,
-                        maxmemory: 1000 * 512
-                    })
-                ]
-            })
-        ],
-    });
-}
+*/
